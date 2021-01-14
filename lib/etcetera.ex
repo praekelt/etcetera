536edb8ab2da0fcd07630133342e839513566398
Wim Keirsgieter <wim@.keirsgieter@gmail.com>
2021-01-14 10:25:44 +0200

    Implement set, get, exists?, delete and rmdir functions (with tests)

--- lib/etcetera.ex
+++ lib/etcetera.ex
@@ -1,18 +1,244 @@
 defmodule Etcetera do
   @moduledoc """
-  Documentation for `Etcetera`.
+  Functions for interacting with an Etcd store.
   """
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  alias Etcetera.Utils
 
-      iex> Etcetera.hello()
-      :world
+  # Etcd error codes
+  @err_not_a_file 102
+  @err_dir_not_empty 108
 
-  """
-  def hello do
-    :world
+  #############################################################################
+  # Etcd wrapper functions
+  #
+  # See https://etcd.io/docs/v2/api/
+  #############################################################################
+
+  def set(key, value) when is_map(value) do
+    Enum.each(value, fn {k, v} -> set("#{key}/#{k}", v) end)
   end
+
+  def set(key, value) when is_list(value) do
+    set(key, Jason.encode!(value))
+  end
+
+  def set(key, value) do
+    resp = make_put(key, %{"value" => value})
+    case resp.status_code do
+      200 ->
+        :ok
+      201 ->
+        :ok
+      401 ->
+        err_msg = "The request requires authentication (insufficient credentials)"
+        Logger.error(err_msg)
+        {:error, err_msg}
+      403 ->
+        body = Jason.decode!(resp.body)
+        err_msg = case body["errorCode"] do
+          @err_not_a_file ->
+            "Could not set key #{key}, already exists as directory"
+          err_code ->
+            "Unhandled error code in set/2: #{err_code}"
+        end
+        Logger.error(err_msg)
+        {:error, err_msg}
+      status_code ->
+        err_msg = "Unhandled status code in set/2: #{status_code}"
+        Logger.error(err_msg)
+        {:error, err_msg}
+    end
+  end
+
+  def get(key, unpack_dir? \\ false) do
+    resp = make_get(key)
+    case resp.status_code do
+      200 ->
+        body = Jason.decode!(resp.body)
+        if body["node"]["dir"] do
+          # Key points to a directory
+          case body["node"]["nodes"] do
+            nil ->
+              # Directory is empty
+              nil
+            nodes ->
+              # Directory is not empty, recursively get elements and build map if unpack_dir option set
+              if unpack_dir? do
+                Enum.reduce(nodes, %{}, fn node, map ->
+                  # Remove leading slash from key
+                  k = String.trim_leading(node["key"], "/")
+
+                  # Remove leading and trailing slashes from prefix
+                  prefix = Utils.remove_slashes(etcd_prefix())
+
+                  # Get rid of the leading prefix or it will be recursively added. Also add it into
+                  # the split or keys with the prefix will not be grabbed properly.
+                  prefix = "#{prefix}/"
+                  k = k
+                  |> String.split(prefix)
+                  |> List.last()
+                  |> String.trim_leading("/")
+
+                  v = get(k, unpack_dir?)
+
+                  # Get rid of preceding directories in key before putting it in the map
+                  k = k
+                  |> String.split("/")
+                  |> List.last()
+                  Map.put(map, k, v)
+                end)
+              else
+                err_msg = "Key '#{key}' points to a directory, try with unpack_dir?: true"
+                Logger.error(err_msg)
+                {:error, err_msg}
+              end
+          end
+        else
+          # Key points to a non-directory value, attempt to JSON-decode it, otherwise return
+          # value as-is
+          value = body["node"]["value"]
+          case Jason.decode(value) do
+            {:ok, result} ->
+              result
+            {:error, _reason} ->
+              value
+          end
+        end
+      401 ->
+        err_msg = "The request requires authentication (insufficient credentials)"
+        Logger.error(err_msg)
+        {:error, err_msg}
+      404 ->
+        Logger.debug("Key '#{key}' not found")
+        nil
+      status_code ->
+        err_msg = "Unhandled status code in get/1: #{status_code}"
+        Logger.error(err_msg)
+        {:error, err_msg}
+    end
+  end
+
+  def exists?(key) do
+    resp = make_get(key)
+    case resp.status_code do
+      200 ->
+        true
+      401 ->
+        err_msg = "The request requires authentication (insufficient credentials)"
+        Logger.error(err_msg)
+        {:error, err_msg}
+      404 ->
+        false
+      status_code ->
+        err_msg = "Unhandled status code in exists?/1: #{status_code}"
+        Logger.error(err_msg)
+        {:error, err_msg}
+    end
+  end
+
+  def delete(key, recursive? \\ true) do
+    resp = make_delete(key)
+    case resp.status_code do
+      200 ->
+        :ok
+      401 ->
+        err_msg = "The request requires authentication (insufficient credentials)"
+        Logger.error(err_msg)
+        {:error, err_msg}
+      403 ->
+        body = Jason.decode!(resp.body)
+        case body["errorCode"] do
+          @err_not_a_file ->
+            rmdir(key, recursive?)
+          err_code ->
+            err_msg = "Unhandled error code in delete/1: #{err_code}"
+            Logger.error(err_msg)
+            {:error, err_msg}
+        end
+      404 ->
+        err_msg = "Key '#{key}' does not exist"
+        Logger.error(err_msg)
+        {:error, err_msg}
+      status_code ->
+        err_msg = "Unhandled status code in delete/1: #{status_code}"
+        Logger.error(err_msg)
+        {:error, err_msg}
+    end
+  end
+
+  def rmdir(dirname, recursive? \\ false) do
+    resp = make_delete(dirname, %{dir: true, recursive: recursive?})
+    case resp.status_code do
+      200 ->
+        :ok
+      401 ->
+        err_msg = "The request requires authentication (insufficient credentials)"
+        Logger.error(err_msg)
+        {:error, err_msg}
+      403 ->
+        body = Jason.decode!(resp.body)
+        case body["errorCode"] do
+          @err_dir_not_empty ->
+            err_msg = "Directory '#{dirname}' not empty, try with recursive?: true"
+            Logger.error(err_msg)
+            {:error, err_msg}
+          err_code ->
+            err_msg = "Unhandled error code in rmdir/1: #{err_code}"
+            Logger.error(err_msg)
+            {:error, err_msg}
+        end
+      404 ->
+        err_msg = "Directory '#{dirname}' does not exist"
+        Logger.error(err_msg)
+        {:error, err_msg}
+      status_code ->
+        err_msg = "Unhandled status code in rmdir/1: #{status_code}"
+        Logger.error(err_msg)
+        {:error, err_msg}
+    end
+  end
+
+  #############################################################################
+  # Private utility functions
+  #############################################################################
+
+  defp make_get(path, params \\ %{}) do
+    make_request(:get, path, params)
+  end
+
+  defp make_put(path, params \\ %{}) do
+    make_request(:put, path, params)
+  end
+
+  defp make_delete(path, params \\ %{}) do
+    make_request(:delete, path, params)
+  end
+
+  defp make_request(method, path, params) do
+    url = Utils.get_etcd_url(etcd_host(), etcd_port(), etcd_prefix(), path)
+    auth = [basic_auth: {etcd_user(), etcd_pass()}]
+
+    Logger.debug("Making request to #{url} with params #{inspect(params)}")
+    case HTTPoison.request(method, url, "", [], [
+      params: params,
+      hackney: auth,
+      follow_redirect: true]
+    ) do
+      {:error, reason} ->
+        Logger.error("Unable to make request to Etcd: #{reason}")
+        %{status_code: 503}
+      {:ok, resp} ->
+        Logger.debug("Response code: #{resp.status_code}")
+        resp
+    end
+  end
+
+  defp etcd_host, do: Application.get_env(:etcetera, :etcd_host)
+  defp etcd_port, do: Application.get_env(:etcetera, :etcd_port)
+  defp etcd_user, do: Application.get_env(:etcetera, :etcd_user)
+  defp etcd_pass, do: Application.get_env(:etcetera, :etcd_pass)
+  defp etcd_prefix, do: Application.get_env(:etcetera, :etcd_prefix)
 end
--- test/etcetera_test.exs
+++ test/etcetera_test.exs
@@ -2,7 +2,76 @@ defmodule EtceteraTest do
   use ExUnit.Case
   doctest Etcetera
 
-  test "greets the world" do
-    assert Etcetera.hello() == :world
+  setup do
+    if Etcetera.exists?("foo") do
+      Etcetera.delete("foo")
+    end
+    {:ok, %{}}
+  end
+
+  describe "test set, exists? and get functions" do
+    test "set and get string value" do
+      assert Etcetera.set("foo", "bar") == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo") == "bar"
+    end
+
+    test "set and get int value" do
+      assert Etcetera.set("foo", 1) == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo") == 1
+    end
+
+    test "set and get float value" do
+      assert Etcetera.set("foo", 3.14) == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo") == 3.14
+    end
+
+    test "set and get boolean value" do
+      assert Etcetera.set("foo", true) == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo") == true
+    end
+
+    test "set and get list value" do
+      assert Etcetera.set("foo", [1, 2, 3]) == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo") == [1, 2, 3]
+    end
+
+    test "set and get map value" do
+      assert Etcetera.set("foo", %{"bar" => "baz"}) == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo", unpack_dir: true) == %{"bar" => "baz"}
+      assert Etcetera.get("foo/bar") == "baz"
+    end
+
+    test "set and get JSON value" do
+      assert Etcetera.set("foo", "{\"key\": \"val\"}") == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo") == %{"key" => "val"}
+    end
+
+    test "set and get complicated value" do
+      value = %{
+        "key" => "val",
+        "int" => 1,
+        "inner" => %{
+          "another" => "key",
+        },
+        "other" => %{
+          "another" => "key",
+        },
+        "list" => [1, "str", 3.14],
+      }
+      assert Etcetera.set("foo", value) == :ok
+      assert Etcetera.exists?("foo")
+      assert Etcetera.get("foo", unpack_dir: true) == value
+    end
+
+    test "get non-existent key" do
+      assert Etcetera.get("foo") == nil
+    end
   end
 end
